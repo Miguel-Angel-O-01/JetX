b2e30a5553ae733d1ad29f56e707375cc57fa1ad
Pedro Ricardo Orantes Cuellar <ricardoorantes16sv@gmail.com>
2026-09-17 11:38:03 -0600

    feat: ocultar contraseña en login

--- app/src/main/java/com/example/jetx/presentation/screens/login/LoginScreeen.kt
+++ app/src/main/java/com/example/jetx/presentation/screens/login/LoginScreeen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -42,6 +43,7 @@ fun LoginScreen(
             value = contraseña,
             onValueChange = { contraseña = it },
             label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth()
         )
 
